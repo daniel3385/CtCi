e7f19c5933ece14c95d8c0b9ccd2841c288f85d0
daniel <daniel.cabral88@gmail.com>
2016-10-10 20:09:46 -0300

    ex 02 done

--- 01_ArrayAndStrings/check_permutation.c
+++ 01_ArrayAndStrings/check_permutation.c
@@ -13,26 +13,27 @@ typedef int Result;
 
 static void sort_string(char s[], int n)
 {
-    printf("sort_string ->%s\n", s1);
     char tmp;
     int j,k;
     for(j = 1; j < n; j++){
         tmp = s[j];
         k = j-1;
-        while(k >= 0 && strcmp((char)tmp, s[k]) < 0){
+        while(k >= 0 && tmp < s[k]){
             s[k+1] = s[k];
             k = k-1;
         }
+        s[k+1] = tmp;
     }
-    s[k+1] = tmp;
 }
 
 static Result check_permutation(char s1[], char s2[])
 {
     Result result = OK;
-    sort_string(s1, sizeof(s1));
+    int len1 = strlen(s1);
+    int len2 = strlen(s2);
+    sort_string(s1, len1);
     printf("String 1->%s\n", s1);
-    sort_string(s2, sizeof(s2));
+    sort_string(s2, len2);
     printf("String 2->%s\n", s2);
     while(*s1 != '\0')
         if(*s1++ != *s2++)
@@ -42,9 +43,9 @@ static Result check_permutation(char s1[], char s2[])
 
 int main()
 {
+    //TODO TDD
     char s1[] = "daniel"; 
     char s2[] = "dnaile";
-    int a = sizeof(s1);
     Result result;
     result = check_permutation(s1, s2);
     if (result == OK)
