f6f020d051431eea8b3ce06611b9b3a494234523
daniel <daniel.cabral88@gmail.com>
2016-10-12 11:53:16 -0300

    need space filled State

--- 01_ArrayAndStrings/urlify.c
+++ 01_ArrayAndStrings/urlify.c
@@ -33,11 +33,18 @@ void replace_space(char s[])
     NumberCharacters number_characters = count_characters(s);
     len = strlen(s); 
     realloc_space(s, number_characters); 
-    while(number_characters >=0 ){
+    while(number_characters >= 0 ){
         if (s[len-1] == ' '){
-            s[number_characters-1] = 
-        s[number_characters-1] = s[len-1];
-
+            s[number_characters-1] = '0';
+            s[number_characters-2] = '2';
+            s[number_characters-3] = '%';
+            number_characters = number_characters - 3;
+            len = len - 1;
+        } else{
+            s[number_characters-1] = s[len-1];
+            len = len - 1;
+        }
+    }
     for(int i=0; i != '\0'; i++){
 
 
