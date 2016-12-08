5afd2a1e0a3813b32980c78633c234d1a27b2bd0
daniel <daniel.cabral88@gmail.com>
2016-12-08 19:45:28 -0200

    create exercise 1 chapter 2

---
+++ 02_LinkedLists/remove_dups.c
@@ -0,0 +1,13 @@
+/*
+ * Remove duplicate from a unsorted linked list
+ */
+
+
+
+int main(){
+    // create linked list
+    // print list
+    // remove dups
+    // print list again
+    return 0;
+}
